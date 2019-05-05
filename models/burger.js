5cb116c633b1001a881955681d59f6717415f244
mathurdevanshi <mathurdevanshi@gmail.com>
2019-05-04 18:37:37 -0700

    modified burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,46 +1,24 @@
-var ORM = require  ("config/orm.js");
-var CONNECTION = require  ("config/connection.js");
-
-//this will create a connection to Heroku
-const PORT = porcess.env.PORT|| 5000;
-const server = http.createServer((req,res) => {
-    res.StatusCode = 200;
-    res.setHeader ('conent-Type', 'text/plain');
-    res.end('Hello World\n')
-});
-server.listen(PORT, () => {
-    console.log('Server running pn ${PORT}/')
-});
-
-//this will connect to mysql
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "webuser",
-    password: "UCR",
-    database: "personal_journal"
-});
-connection.connect(function(err){
-    if(err){
-        console.log("This is bad. Run now. Run far and fast. It is most likely already behind you.");
-        return;
+module.exports= function(){
+    //import orm
+    var ormFile = require("config/orm.js");
+    
+    var burger = {
+        all: function(cb){
+            ormFile.selectAll('burgers', function(res){
+                cb(res);
+            });
+        },
+        create: function(col, burger_name, cb){
+            ormFile.insertOne('burgers', burger_name, function(res){
+                cb(res);
+            });
+        },
+        update: function(objColVals, condition, cb){
+            ormFile('burgers', objColVals, condition, function(res){
+                cb(res);
+            });
+        }
     }
-    console.log("MySQL connected: " + connection.threadId);
-});
+    
+};
 
-//this will create a connection to the ORM file
-new ORMfunctions = new ORM();
-
-//this will listen for the ports
-app.listen(PORT, function(){
-    console.log("Web server is running on port: ", PORT);
-});
-
-//this will call the insertOne() function:
-ORMfunctions.insertOne();
-
-//this will call the selectAll() function: 
-ORMfunctions.selectAll();
-
-//this will call the updateOne() function:
-ORMfunctions.updateOne();
