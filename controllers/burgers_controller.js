b1aff6f83be63d4c9df7944d992274095dcc0a00
mathurdevanshi <mathurdevanshi@gmail.com>
2019-05-04 18:08:58 -0700

    burgers_controller has gotten its facelift

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,18 +1,30 @@
-module.exports= function(){
-    //import orm
-    var ormFile = require("config/orm.js");
-    //createNewItem
-    this.createNewItem= function(typedValue){
-        var sqlResults = ormFile.insertOne("burgers", "burger_name", "devoured", typedValue, false);
-        var allSqlResults = ormFile.selectAll("burgers");
-        return (sqlResults, allSqlResults);
-    }
-    //devourItem
-    this.devourItem = function(id){
-        var sqlResults = ormFile.updateOne("burgers", true, id);
-        var allSqlResults = ormFile.selectAll("burgers");
-        return (sqlResults, allSqlResults);
-    }
-    
-};
+module.exports = function (){
+    // do all the importing npm files
+    var express = require('express');
+    var bodyParser=require('body-parser');
+    var methodOverride= require('method-override');
+    var router = express.Router();
+    var burger = require('models/burger.js');
 
+    // routing
+    router.get('/', function (req, res){
+        res.redirect('/burgers');
+    });
+    router.get('/burgers', function(req, res){
+        burger.all(function(data){
+            var hbsObject = {burgers:data};
+            res.render('index', hbsObject);
+        });
+    });
+    router.post('/burgers/create',function(req,res){
+        burger.create('burger_name', req.body.burger_name, function(){
+            res.redirect('/burgers');
+        });
+    });
+    router.put('/burgers/update/:id', function(req, res){
+        var condition = 'id =' + req.params.id;
+        burger.update({devoured: req.body.devoured}, condition, function(){
+            res.redirect('/burgers');
+        });
+    })
+}
